ae5cf1981f13a9bd93ca9e6b5035ecdf56269e20
Nathan Myers <nathanemyers@gmail.com>
2015-12-08 17:37:37 -0500

    adding flask backend test

---
+++ 2015/src/backend.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+# return team details
+@app.route('/teams', methods=['GET'])
+def get_teams():
+    return 'returning all teams'
+
+# return team details
+@app.route('/teams/<team_name>', methods=['GET'])
+def get_team(team_name):
+    return team_name + '!'
+
+# return rankings
+@app.route('/rankings', methods=['GET'])
+def get_rankings():
+    start_week = request.args.get('start-week')
+    end_week = request.args.get('end-week')
+    return 'Get the rankings!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
