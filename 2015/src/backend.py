a9d27b1858d8390c8ac14fc97334edba61fe4288
Nathan Myers <nathanemyers@gmail.com>
2015-12-09 14:20:25 -0500

    adding some db hooks to the backend script

--- 2015/src/backend.py
+++ 2015/src/backend.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
