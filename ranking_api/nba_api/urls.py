cd275cd937c4edc11b39d654a228bd143cd3592d
Nathan Myers <nathanemyers@gmail.com>
2016-01-30 12:02:07 -0600

    adding gut check help call

--- ranking_api/nba_api/urls.py
+++ ranking_api/nba_api/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
         url(r'^$', views.help),
+        url(r'^help$', views.help),
         url(r'^rankings/(?P<year>[0-9]+)/(?P<week>[0-9]+)', views.week_rankings),
         url(r'^rankings/(?P<year>[0-9]+)', views.year_rankings),
         url(r'^rankings/info', views.info),
