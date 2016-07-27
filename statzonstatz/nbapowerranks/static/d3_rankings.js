509cc6e35d8208c1301e80a7f3eb5e28b2c69383
Nathan Myers <nathanemyers@gmail.com>
2016-07-27 12:10:06 -0500

    working on scrolling margins

--- statzonstatz/nbapowerranks/static/d3_rankings.js
+++ statzonstatz/nbapowerranks/static/d3_rankings.js
@@ -1,22 +1,26 @@
 var data;
-var margin = {top: 0, right: 0, bottom: 30, left: 0};
+
+var margin = {top: 0, right: 0, bottom: 0, left: 100};
 var width = 960 - margin.left - margin.right;
 var height = 600 - margin.top - margin.bottom;
 
+var dataMargin = {top: 10, right: 6, bottom: 50, left: 0};
+var dataWidth = width - dataMargin.left - dataMargin.right;
+var dataHeight = height - dataMargin.top - dataMargin.bottom;
 
-var svg;
 var x_min = 0;
 var x_max = 10;
 
 var x = d3.scaleLinear()
   .domain([x_min, x_max])
-  .range([0, width]);
+  .range([dataMargin.left, dataWidth]);
 
 var y = d3.scaleLinear()
   .domain([1, 30])
-  .range([0, height]);
+  .range([dataMargin.top, dataHeight]);
 
 var line = d3.line()
+  .curve(d3.curveMonotoneX)
   .x(function(d) {return x(d.week);})
   .y(function(d) {return y(d.rank) ;});
 
@@ -29,20 +33,26 @@ window.onload = function() {
     data = json.results;
     init();
 
-    svg = d3.select(".chart").append("svg")
+
+    var outer = d3.select(".chart").append("svg")
         .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("height", height + margin.top + margin.bottom);
+
+    var inner = outer.append("svg")
+        .attr("width", dataWidth + dataMargin.left + dataMargin.right)
+        .attr("height", dataHeight + dataMargin.top + dataMargin.bottom)
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      .append('g')
+        .attr("transform", "translate(" + dataMargin.left + "," + dataMargin.top + ")");
+        
 
     var xAxis = d3.axisBottom(x);
 
-    var gX = svg.append('g')
-      .attr('class', 'axis axis--x')
-      .attr('transform', 'translate(0,' + (height + 10) + ')')
+    var gX = outer.append('g')
+      .attr('transform', 'translate(' + dataMargin.left + ',' + (height - margin.bottom - dataMargin.bottom + 5) + ')')
       .call(xAxis);
 
-    var team = svg.selectAll('.team')
+    var team = inner.selectAll('.team')
       .data(data)
       .enter().append('g')
         .attr('class', 'team');
@@ -54,21 +64,35 @@ window.onload = function() {
       .style('fill', 'none')
       .style('stroke', 'blue');
 
-    svg.append('rect')
+    var zoom = d3.zoom()
+      .on('zoom', zoomed)
+      //.on('end', center)
+      .translateExtent([[0,0], [3200, height + margin.top + margin.bottom]])
+      .scaleExtent([0,0]);
+
+    inner.append('rect')
       .attr('width', width)
       .attr('height', height)
       .attr('fill', 'none')
       .attr('pointer-events', 'all')
-      .call(d3.zoom()
-            .on('zoom', zoomed)
-            .translateExtent([[0,0], [3200, 600]])
-            .scaleExtent([0,0])
-           );
+      .call(zoom);
 
     function zoomed() {
       team.attr('transform', d3.event.transform);
       gX.call(xAxis.scale(d3.event.transform.rescaleX(x)));
     }
+    
+    var format = d3.format(".01f");
+    function center() {
+      var base = format(x.invert(-d3.event.transform.x));
+      roundedBase = format(Math.round(base));
+      if (base == roundedBase) {
+        return;
+      }
+      zoom.translateBy(team, base - roundedBase);
+    }
+    d3.select('#center')
+      .on('click', center);
 
   });
 
